42c3b3bd81cbb8ba4c77790b9a6f5da1ccae56eb
Prince Addo <princeaddo1199@gmail.com>
2024-03-22 12:48:30 +0000

    createBoard function to show the grid divs created using javascript

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-  creatBoard(16);
+  creatBoard(32);
 });
 
 function creatBoard(size) {
   let board = document.querySelector(".board");
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-  console.log("hi");
+
+  let numOfDivs = size * size;
+
+  for (let i = 0; i < numOfDivs; i++) {
+    let div = document.createElement("div");
+    div.style.border = "1px solid black";
+    board.insertAdjacentElement("beforeend", div);
+  }
 }
